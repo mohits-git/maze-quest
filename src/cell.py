afee095f98d6471616393b3ad5184bd9ac8df5d5
mohits-git <mohitkhatri2805@gmail.com>
2024-07-23 21:20:59 +0530

    feat: add draw_move method in Cell class to draw paths between cells

--- src/cell.py
+++ src/cell.py
@@ -1,3 +1,4 @@
+from typing import Self
 from graphics import Line, Point, Window
 
 
@@ -37,3 +38,23 @@ class Cell:
         if self.has_bottom_wall:
             tw = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(tw)
+
+    def draw_move(self, to_cell: Self, undo=False):
+        fill_color = "gray" if undo else "red"
+        if (
+            self._x1 is None or self._x2 is None
+            or self._y1 is None or self._y2 is None
+            or to_cell._x1 is None or to_cell._x2 is None
+            or to_cell._y1 is None or to_cell._y2 is None
+        ):
+            raise ValueError("Cells should be drawn first")
+        cell1_center = Point(
+            self._x1 + abs(self._x2 - self._x1)//2,
+            self._y1 + abs(self._y2 - self._y1)//2
+        )
+        cell2_center = Point(
+            to_cell._x1 + abs(to_cell._x2 - to_cell._x1)//2,
+            to_cell._y1 + abs(to_cell._y2 - to_cell._y1)//2
+        )
+        line = Line(cell1_center, cell2_center)
+        self._win.draw_line(line, fill_color)
--- src/main.py
+++ src/main.py
@@ -6,25 +6,23 @@ def main():
     win = Window(width=800, height=600)
 
     cell1 = Cell(win)
+    cell1.has_left_wall = False
     cell2 = Cell(win)
-    cell2.has_left_wall = False
     cell3 = Cell(win)
-    cell3.has_left_wall = False
-    cell3.has_top_wall = False
     cell4 = Cell(win)
-    cell4.has_left_wall = False
-    cell4.has_top_wall = False
-    cell4.has_right_wall = False
     cell5 = Cell(win)
     cell5.has_bottom_wall = False
-    cell5.has_top_wall = False
-    cell5.has_right_wall = False
 
     cell1.draw(50, 50, 100, 100)
     cell2.draw(150, 50, 200, 100)
     cell3.draw(250, 50, 300, 100)
     cell4.draw(350, 50, 400, 100)
-    cell5.draw(450, 50, 500, 100)
+    cell5.draw(250, 150, 300, 200)
+
+    cell1.draw_move(cell2)
+    cell2.draw_move(cell3)
+    cell4.draw_move(cell3, undo=True)
+    cell3.draw_move(cell5)
 
     win.wait_for_close()
 
